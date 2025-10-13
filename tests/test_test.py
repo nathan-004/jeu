f6d5c17209ad57f054e703d92a11040c181ae7a2
Nathan <n49874687@gmail.com>
2025-10-13 19:53:07 +0200

    tests `unittest`

---
+++ tests/test_test.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestTest(unittest.TestCase):
+    def test_function(self):
+        self.assertEqual(2+3, 5)
+
+    def test_function2(self):
+        self.assertEqual(5-3, 2)
