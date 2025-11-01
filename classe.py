a78eb4891b7762ee03a9edb8186f794ac6761953
Nathan <n49874687@gmail.com>
2025-11-01 14:19:28 +0100

    ajout de bordures aux statistiques

--- classe.py
+++ classe.py
@@ -533,7 +533,9 @@ class Game:
                 stats = self.personnage.get_stats_message()
                 if stats != debug_text.txt:
                     debug_text = TextDisplay(stats, self.screen, self.clock, background_color=(0,0,0), color=(255,255,255), pos=debug_text_pos, size=debug_text_size)
+                borders = pygame.Rect(debug_text_pos, debug_text_size)
                 debug_text.display(1)
+                pygame.draw.rect(self.screen, "white", borders, 2)
                 pygame.display.flip()
                 continue
 
